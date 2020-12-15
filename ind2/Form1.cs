cfdb2f7d880b69bdd15e4fe22725f471415ea680
Akopdzhanova Nicole <nic.a@mail.ru>
2020-12-15 20:38:51 +0300

    Added reflective materials, shadows & reflections

--- ind2/Form1.cs
+++ ind2/Form1.cs
@@ -25,12 +25,14 @@ namespace ind2
             height = image.Height;
             camera_pos = new Vec3(0, 0, 10);
 
-            lights = new List<Light>();
             spheres = new List<Sphere>();
-            spheres.Add(new Sphere(new Vec3(0, 0, 0), 2.0, new Material(new Vec3(20, 20, 20))));
-            spheres.Add(new Sphere(new Vec3(0, 5, 0), 3.0, new Material(new Vec3(20, 20, 20))));
-            spheres.Add(new Sphere(new Vec3(-3, -1, -2), 4.0, new Material(new Vec3(20, 20, 20))));
-            lights.Add(new Light(new Vec3(10, 7, 20), 5));
+            spheres.Add(new Sphere(new Vec3(0, 0, 0), 2.0, new Material(new Vec3(255, 150, 255), 0.6, 0.1, 0.6, 0.1, 0.1)));
+            spheres.Add(new Sphere(new Vec3(0, 5, 0), 3.0, new Material(new Vec3(255, 150, 255), 0.9, 0.3, 0.6, 0.1, 5)));
+            spheres.Add(new Sphere(new Vec3(-5, -1, -2), 4.0, new Material(new Vec3(255, 150, 255), 0.6, 0.3, 0.6, 0.1, 10.0)));
+
+            lights = new List<Light>();
+            lights.Add(new Light(new Vec3(10, 7, 30), 1.0));
+            lights.Add(new Light(new Vec3(-10, -20, 20), 1.0));
         }
 
         private void image_Click(object sender, EventArgs e)
@@ -51,6 +53,12 @@ namespace ind2
                     double y = -(2 * (j + 0.5) / (double)height - 1) * Math.Tan(fov / 2.0);
                     Vec3 dir = new Vec3(x, y, -1).normalize();
                     Vec3 c = cast_ray(new Ray(camera_pos, dir));
+                    if (c.x > 255)
+                        c.x = 255;
+                    if (c.y > 255)
+                        c.y = 255;
+                    if (c.z > 255)
+                        c.z = 255;
 
                     bmp.SetPixel(i, j, Color.FromArgb(255, (int)c.x, (int)c.y, (int)c.z));
                 }
@@ -58,20 +66,45 @@ namespace ind2
             image.Image = bmp;
         }
 
-        public Vec3 cast_ray(Ray ray)
+        public Vec3 cast_ray(Ray ray, int depth = 0)
         {
             Vec3 point = new Vec3(), N = new Vec3();
-            Material m = new Material(new Vec3(255, 0, 0));
-            if (!scene_intersect(ray, ref m, ref point, ref N))
+            Material m = new Material(new Vec3(255, 0, 0), 1, 1, 1, 0);
+            if (depth > 4 || !scene_intersect(ray, ref m, ref point, ref N))
                 return new Vec3(150, 150, 255);
 
-            double diffuse_light_intensity = 0;
+            Vec3 reflect_dir = reflect(ray.direction, N).normalize();
+            Vec3 reflect_orig = reflect_dir * N < 0 ? point - N * 1e-3 : point + N * 1e-3;
+            Ray reflect_ray = new Ray(reflect_orig, reflect_dir);
+            Vec3 reflect_color = cast_ray(reflect_ray, depth + 1);
+            double diffuse_light_intensity = 0, specular_light_intensity = 0;
             for (int i = 0; i < lights.Count(); i++)
             {
                 Vec3 light_dir = (lights[i].position - point).normalize();
+
+                double light_distance = (lights[i].position - point).Length();
+
+                Vec3 shadow_orig = light_dir * N < 0 ? point - N * 1e-3 : point + N * 1e-3; 
+                Vec3 shadow_pt = new Vec3(), shadow_N = new Vec3();
+                Material tmpmaterial = new Material();
+                Ray shadow_ray = new Ray(shadow_orig, light_dir);
+                if (scene_intersect(shadow_ray, ref tmpmaterial, ref shadow_pt, ref shadow_N) && (shadow_pt - shadow_orig).Length() < light_distance)
+                    continue;
+
                 diffuse_light_intensity += lights[i].intensity * Math.Max(0.0, light_dir * N);
+                specular_light_intensity += Math.Pow(Math.Max(0.0, reflect(light_dir, N) * ray.direction), m.specular_value)
+                    * lights[i].intensity;
             }
-            return m.color * diffuse_light_intensity;
+            if (specular_light_intensity > 1)
+                specular_light_intensity = 1;
+            return m.color * diffuse_light_intensity * m.diffuse_albedo + 
+                new Vec3(255, 255, 255) * specular_light_intensity * m.specular_albedo +
+                reflect_color * m.reflectiveness_albedo;
+        }
+
+        public Vec3 reflect(Vec3 I, Vec3 N)
+        {
+            return I - N * 2.0 * (I * N);
         }
 
         public bool scene_intersect(Ray ray, ref Material material, ref Vec3 hit, ref  Vec3 N)
--- ind2/Material.cs
+++ ind2/Material.cs
@@ -9,9 +9,20 @@ namespace ind2
     public class Material
     {
         public Vec3 color;
-        public Material(Vec3 _color) 
+        public double diffuse_albedo;
+        public double specular_albedo;
+        public double transparency_albedo;
+        public double reflectiveness_albedo;
+        public double specular_value;
+        public Material(Vec3 _color, double _diffuse_albedo, double _specular_albedo, double _reflectiveness_albedo, double _transparency_albedo, double _specular_value = 0.5) 
         {
             color = _color;
+            diffuse_albedo = _diffuse_albedo;
+            specular_albedo = _specular_albedo;
+            transparency_albedo = _transparency_albedo;
+            reflectiveness_albedo = _reflectiveness_albedo;
+
+            specular_value = _specular_value;
         }
         public Material()
         {
